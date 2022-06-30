2bcf6663b5cd933b2377d1789c2f66476240f6e7
Ameermu <ameermuhd01234@github.com>
2022-06-30 03:21:18 -0700

    Modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *   *
  *    *Description: We are unable to use printf so must use put to print this string
  *     *Return: Code should return a 0
- *      */
+ *     
+ * 	*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
