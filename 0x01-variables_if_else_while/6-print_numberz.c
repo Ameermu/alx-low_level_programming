efb8c030fe33543019f744af9664bbe4383c4688
Ameermu <ameermuhd01234@github.com>
2022-07-01 17:34:43 -0700

    Modified

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,17 @@
  *   *
  *    *Description: Prints 0-10 using putchar function and no char variables
  *     *Return: 0
- *      */
+ */
 int main(void)
 {
-		int a = 0;
+	int a = 0;
 
-			while (a < 10)
-					{
-								putchar('0' + a);
-										a++;
-											}
+	while (a < 10)
+	{
+		putchar('0' + a);
+		a++;
+	}
 
-				putchar('\n');
-					return (0);
+	putchar('\n');
+	return (0);
 }
