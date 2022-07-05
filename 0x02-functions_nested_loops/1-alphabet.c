237206dd027383bfcfb9bdabd45fef1253ca307f
Ameermu <ameermuhd01234@github.com>
2022-07-05 15:54:41 -0700

    Test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,19 +3,17 @@
 /**
  * print_alphabet - prints the alphabet in lowercase,
  * followed by a new line.
- * Return: 0.
+ * Return: return void.
  */
 void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++);
-
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar (i);
 	}
 
-	_putchar ('\n');
+		_putchar ('\n');
 
-	return (0);
 }
