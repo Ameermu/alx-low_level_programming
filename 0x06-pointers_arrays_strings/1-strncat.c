4b58adb66128b651786b217e770ba913aee7dde8
Ameermu <ameermuhd01234@github.com>
2022-07-15 02:31:55 -0700

    test

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,20 +10,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len1 = 0; /* string length for src */
-	int len2 = 0; /* string length for dest */
-	int i;
+	strncat(dest, src, n)
 
-	while (src[len1] != '\0')
-		len1++;
-
-	while (dest[len2] != '\0')
-		len2++;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[len2] = src[i];
-		len2++;
-	}
 	return (dest);
 }
