be6e94674c762c416a4eff8a8143c91f364bf63b
Ameermu <ameermuhd01234@gmail.com>
2022-08-10 10:40:42 -0700

    cleaned code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,4 +10,32 @@
 
 char *cap_string(char *)
 {
+	int i = 0, j, flag;
+	char wordSep[] = ",\t;\n; .!?\"(){}";
+
+	do {
+		flag = 0;
+
+		if (i == 0)
+			flag = 1;
+		else
+		{
+			for (j = 0; wordSep[j] != '\0'; j++)
+			{
+				if (str[i - 1] == wordSep[j])
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+		if (flag == 1 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		i++;
+
+	} while (str[i] != '\0');
+
+	return (str);
+}
 
