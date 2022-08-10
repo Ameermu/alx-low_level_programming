dbf4290be67bc1faf57085eb392e0317c37b3196
Ameermu <ameermuhd01234@gmail.com>
2022-08-09 23:45:48 -0700

    cleanedcode

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,35 @@
-/*
- * FILE: 101-print_comb4.c
- * DEV: Ameer Muhammad
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits,
- * 	in acending order, separated by a comma followed by a space.
- *
- * 	Return: 0
+ * main - output all single digit number of base 10
+ * REturn: 0 if successful
  */
-int main (void)
+
+int main(void)
 {
-	int digit1, digit2, digit3;
+	int var;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (var = 0; var < 8; var++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		int ii;
+
+		for (ii = var + 1; ii < 9; ii++)
 		{
-			(digit3 = digit2 + 1; digit3 < 10; digit3++)
+			int jj;
+
+			for (jj = ii + 1; jj < 10; jj++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar((var % 10) + '0');
+				putchar((ii % 10) + '0');
+				putchar((jj % 10) + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+				if (var != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
 					continue;
-
-				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
