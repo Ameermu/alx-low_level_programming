33aa4cdcb89a7e6ab9cde6e995720c468a2ec0d1
Ameermu <ameermuhd01234@gmail.com>
2022-08-11 10:19:51 -0700

    cleaned code

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-#include <unistd.h>
-
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
