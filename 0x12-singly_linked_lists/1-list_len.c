6dcc2739b285835d4faa44455250d23f25aa9fda
Ameermu <ameermuhd01234@gmail.com>
2022-08-11 10:26:53 -0700

    cleaned code

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: the linked list to check
@@ -7,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-	int counter = 0;
+	int count = 0;
 
 	while (h)
 	{
+		count++;
 		h = h->next;
-		counter++;
 	}
 	
-	return (counter);
+	return (count);
 }
