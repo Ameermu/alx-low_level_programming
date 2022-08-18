1bc9c788fdd7697ef935f566e3796de9c3a0f2d6
Ameermu <ameermuhd01234@gmail.com>
2022-08-18 07:11:50 -0700

    files

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * clear_bit - Sets the value of a bit at a given index to 0.
  * @n: A pointer to the bit.
  * @index: The index to set the value at - indices start at 0.
@@ -16,4 +15,4 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	*n &= ~(1 << index);
 
 	return (1);
-}|
+}
